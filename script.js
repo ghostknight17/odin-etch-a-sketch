81592439ac84b47a0fd496dce06f094a63d98aed
ghostknight17 <125161227+ghostknight17@users.noreply.github.com>
2023-04-09 21:03:04 -0300

    Refactor canvas creation

--- script.js
+++ script.js
@@ -1,13 +1,25 @@
 const container = document.querySelector('.container');
 const resetButton = document.querySelector('.reset-button');
+let pickedNum = 16;
 let squareDiv;
-for (let i = 0; i < 256; i++) {
-  squareDiv = document.createElement('div');
-  container.appendChild(squareDiv);
-  squareDiv.addEventListener('mouseover', (e) => {
+
+createCanvas();
+
+resetButton.addEventListener('click', () => {
+  pickedNum = prompt('how many squares per side?', 16);
+  createCanvas();
+});
+
+function createCanvas() {
+  for (let i = 0; i < pickedNum ** 2; i++) {
+    squareDiv = document.createElement('div');
+    container.appendChild(squareDiv);
+    leaveTrace(squareDiv);
+  }
+}
+
+function leaveTrace(element) {
+  element.addEventListener('mouseover', (e) => {
     e.target.classList.add('hover');
   });
 }
-resetButton.addEventListener('click', () => {
-  prompt('how many squares per side?', '16');
-});

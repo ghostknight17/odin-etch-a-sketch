fe57aa11e1f4cf5ccd05ae2dc9e06b66d13d3296
ghostknight17 <125161227+ghostknight17@users.noreply.github.com>
2023-04-09 21:58:39 -0300

    Update canvas creation and square resolution
    The resetButton event listener now prompts the user to input the desired
    number of squares per side, with a maximum of 100 squares. The
    createCanvas function checks if the user input exceeds the maximum, and
    if so, sets the number of squares to 100. the getResolution function
    sets the width and height of each square to be proportional to the
    number of squares per side. THe squareDiv.setAttribute function call in createCanvas was replaced with a call to the new getResolution function.

--- script.js
+++ script.js
@@ -7,14 +7,20 @@ createCanvas();
 
 resetButton.addEventListener('click', () => {
   container.innerHTML = '';
-  pickedNum = prompt('how many squares per side?', 16);
+  pickedNum = Number(
+    prompt(
+      'Enter how many squares per side you want the canvas to be (maximum 100 squares)',
+      `${pickedNum}`
+    )
+  );
   createCanvas();
 });
 
 function createCanvas() {
+  if (pickedNum > 100) pickedNum = 100;
   for (let i = 0; i < pickedNum ** 2; i++) {
     squareDiv = document.createElement('div');
-    squareDiv.setAttribute('style', 'width: 6.25%; height: 6.25%');
+    getResolution(pickedNum);
     container.appendChild(squareDiv);
     leaveTrace(squareDiv);
   }
@@ -24,4 +30,11 @@ function leaveTrace(element) {
   element.addEventListener('mouseover', (e) => {
     e.target.classList.add('hover');
   });
+}
+
+function getResolution(pickedNum) {
+  squareDiv.setAttribute(
+    'style',
+    `width: ${(1 / pickedNum) * 100}%; height: ${(1 / pickedNum) * 100}%`
+  );
 }
